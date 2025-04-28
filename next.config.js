4ac196b45d3838999d4d2da81aaf2076a9ef0428
Nikita Kudenikov <nikita.kudenikov@travelgsm.com>
2025-04-28 11:50:16 +0200

    fixed config 2

--- next.config.js
+++ next.config.js
@@ -1,18 +1,34 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   output: 'export',
-  basePath: '/climatase',
-
+  ...(isProduction && {
+    basePath: '/climatise',
+    assetPrefix: '/climatise/',
+    trailingSlash: true,
+  }),
+  images: {
+    unoptimized: true,
+    loader: 'akamai',
+    path: isProduction
+      ? '/climatise/'
+      : '/',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack']
-    });
-
-    return config;
+      use: ['@svgr/webpack'],
+    })
+    return config
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
